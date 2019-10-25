6ab70cb0e90bb4913355096e9a760291dc8bb00e
yOoMarvin <marvin.messenzehl@gmail.com>
2019-10-26 01:35:10 +0200

    basic routing

--- client-customer/src/components/App/index.js
+++ client-customer/src/components/App/index.js
@@ -7,8 +7,45 @@ import {
   Card,
   CardContainer,
 } from "./style";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/complain">Lass es raus</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profil</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/complain">
+            <Complain />
+          </Route>
+          <Route path="/profile">
+            <Profil />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
   return (
     <Container>
       <Title>
@@ -28,4 +65,18 @@ function App() {
   );
 }
 
-export default App;
+function Complain() {
+  return (
+    <Container>
+      <h2>Lass es raus</h2>
+    </Container>
+  );
+}
+
+function Profil() {
+  return (
+    <Container>
+      <h2>Profil & Einstellungen</h2>
+    </Container>
+  );
+}
