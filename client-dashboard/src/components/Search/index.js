9d2701d38297400370fcb289c7779b1f3de9dc8f
yOoMarvin <marvin.messenzehl@gmail.com>
2019-10-26 17:37:49 +0200

    event handling fail

--- client-dashboard/src/components/Search/index.js
+++ client-dashboard/src/components/Search/index.js
@@ -2,13 +2,27 @@ import React from "react";
 import { SearchBar, SearchContainer, Input } from "./style";
 import { Search as SearchIcon } from "react-feather";
 
-export default function Search() {
-  return (
-    <SearchBar>
-      <SearchContainer>
-        <Input placeholder="Suche nach Tags, Fahrzeugen oder Themen" />
-        <SearchIcon color={"#5C6166"} />
-      </SearchContainer>
-    </SearchBar>
-  );
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <SearchBar>
+        <SearchContainer>
+          <Input
+            placeholder="Suche nach Tags, Fahrzeugen oder Themen"
+            value={this.props.search}
+            onChange={this.props.onChange}
+          />
+          <button
+            onClick={this.props.onClick}
+            style={{ backgroundColor: "white", border: "none" }}
+          >
+            <SearchIcon color={"#5C6166"} />
+          </button>
+        </SearchContainer>
+      </SearchBar>
+    );
+  }
 }
--- client-dashboard/src/containers/Dashboard/index.js
+++ client-dashboard/src/containers/Dashboard/index.js
@@ -13,35 +13,56 @@ import { CardTitle, CardContainer, Card } from "../../components/globals";
 import Category from "../../components/Category";
 import Filter from "../../components/Filter";
 
-export default function Dashboard() {
-  return (
-    <Content>
-      <TitleContainer>
-        <Title>Inbox</Title>
-        <Inbox size={32} color={"#FC642D"} />
-      </TitleContainer>
-      <Search />
-      <FilterContainer>
-        <Filter text="F228122" />
-      </FilterContainer>
-      <HorizontalContainer>
-        <CardContainer>
-          <CardTitle>Offene Meldungen</CardTitle>
-          <Card style={{ width: "350px" }}>
-            <ListItem border label="Tür Defekt" />
-            <ListItem border label="Tür Defekt - Senorik" />
-            <ListItem label="Glasbruch - Fahrer" />
-            <div style={{ marginBottom: "8px" }}></div>
-          </Card>
-        </CardContainer>
-        <CardContainer>
-          <CardTitle>Kategorien</CardTitle>
-          <Category text="Mechanik-Tür" count="3" />
-          <Category text="Kundenmanagement" count="2" />
-          <Category text="Sauberkeit" count="2" />
-          <Category text="Mechanik-Klima" count="1" />
-        </CardContainer>
-      </HorizontalContainer>
-    </Content>
-  );
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: "" };
+  }
+
+  handleSearchClick(event) {
+    event.preventDefault();
+    alert("Test");
+  }
+
+  handleSearchChange(event) {
+    event.preventDefault();
+    this.setState({ search: event.target.value });
+  }
+
+  render() {
+    return (
+      <Content>
+        <TitleContainer>
+          <Title>Inbox</Title>
+          <Inbox size={32} color={"#FC642D"} />
+        </TitleContainer>
+        <Search
+          search={this.state.search}
+          onChange={this.handleSearchChange.bind(this)}
+          onClick={this.handleSearchChange.bind(this)}
+        />
+        <FilterContainer>
+          <Filter text="F228122" />
+        </FilterContainer>
+        <HorizontalContainer>
+          <CardContainer>
+            <CardTitle>Offene Meldungen</CardTitle>
+            <Card style={{ width: "350px" }}>
+              <ListItem border label="Tür Defekt" />
+              <ListItem border label="Tür Defekt - Senorik" />
+              <ListItem label="Glasbruch - Fahrer" />
+              <div style={{ marginBottom: "8px" }}></div>
+            </Card>
+          </CardContainer>
+          <CardContainer>
+            <CardTitle>Kategorien</CardTitle>
+            <Category text="Mechanik-Tür" count="3" />
+            <Category text="Kundenmanagement" count="2" />
+            <Category text="Sauberkeit" count="2" />
+            <Category text="Mechanik-Klima" count="1" />
+          </CardContainer>
+        </HorizontalContainer>
+      </Content>
+    );
+  }
 }
