c82b39099063f35faff67d25c427409a4628930e
yOoMarvin <marvin.messenzehl@gmail.com>
2019-10-26 15:57:48 +0200

    basic history

--- client-dashboard/src/containers/History/index.js
+++ client-dashboard/src/containers/History/index.js
@@ -1,6 +1,33 @@
 import React from "react";
-import {} from "./style";
+import { Content, Title, TitleContainer, HorizontalContainer } from "./style";
+import { BarChart2 } from "react-feather";
+import Search from "../../components/Search";
+import ListItem from "../../components/ListItem";
+import { CardTitle, CardContainer, Card } from "../../components/globals";
 
 export default function History() {
-  return <h1>Hi!</h1>;
+  return (
+    <Content>
+      <TitleContainer>
+        <Title>History</Title>
+        <BarChart2 size={32} color={"#FC642D"} />
+      </TitleContainer>
+      <Search />
+      <HorizontalContainer>
+        <CardContainer>
+          <CardTitle>Bearbeitete Meldungen</CardTitle>
+          <Card style={{ width: "350px" }}>
+            <ListItem></ListItem>
+            <ListItem></ListItem>
+            <ListItem></ListItem>
+            <div style={{ marginBottom: "32px" }}></div>
+          </Card>
+        </CardContainer>
+        <CardContainer>
+          <CardTitle>Kategorien</CardTitle>
+          <Card>Hier lebt ein Graf</Card>
+        </CardContainer>
+      </HorizontalContainer>
+    </Content>
+  );
 }
